d924e45f28bfbcc90d7e99f821808462f6c32bce
vladis <br.vladis.dev@gmail.com>
2016-10-12 00:32:15 +0300

    get version, timer, timer handler

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,34 @@
+﻿ 
+import time
+
+from win32api import GetFileVersionInfo, LOWORD, HIWORD
+
+def get_version_number (filename):
+  info = GetFileVersionInfo (filename, "\\")
+  ms = info['FileVersionMS']
+  ls = info['FileVersionLS']
+  return HIWORD (ms), LOWORD (ms), HIWORD (ls), LOWORD (ls)
+
+
+def TimerHandler():
+    print CheckVersion()     
+    Timer()
+
+def Timer():
+    time.sleep(1)
+    TimerHandler()
+
+def CheckVersion():          
+    filename =r'C:\Users\tenom\Documents\Visual Studio 2015\Projects\TPVC_App\TPVC_App\bin\Release\TPVC_App.exe' 
+    return ".".join ([str (i) for i in get_version_number (filename)])
+
+Timer()
+
+
+
+
+
+
+
+
+ 
